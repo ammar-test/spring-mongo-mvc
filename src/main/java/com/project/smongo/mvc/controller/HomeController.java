8e7ce7492f08d6df2eeb6ce9d09ef697c42d1d8f
Ammar Iyad <test.acm2011@gmail.com>
2018-03-13 08:54:54 +1300

    make it better

---
+++ src/main/java/com/project/smongo/mvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.project.smongo.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String showHome(){
+		return "home Test";
+	}
+}
